9612f110f34d068e57c5d796a97b47878660cb80
Atharva Kale <112189075+KaleAtharva@users.noreply.github.com>
2023-09-09 19:47:04 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -3,5 +3,6 @@ class Hello{
     System.out.println("DevOps Connect");
     System.out.println("Hello PVGians");
     System.out.println("Hello KAVISHWAR");
+    System.out.println("Just checking");
   }
 }
